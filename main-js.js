ef8bf2c0bb9e72218ac33b5777fa4d9831d45c38
Oksana Chorna <ksuchorna@gmail.com>
2021-01-28 23:30:53 +0200

    1, 2 зроблені

--- main-js.js
+++ main-js.js
@@ -1,7 +1,5 @@
 import galleryItems from "./gallery-items.js";
 
-const galleryRef = document.querySelector(".js-gallery");
-
 const createGalleryItem = ({ preview, original, description }) =>
   `<li class="gallery__item"><a class="gallery__link" href='${original}' ><img class="gallery__image" src='${preview}' data-source='${original}' alt='${description}' /></a></li>`;
 
@@ -10,9 +8,21 @@ const galleryMarkup = galleryItems.reduce(
   ""
 );
 
+const galleryRef = document.querySelector(".js-gallery");
+
 galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
 
 galleryRef.addEventListener("click", onImgClick);
+
+const largeImgRef = document.querySelector(".gallery__link");
+
 function onImgClick(event) {
-  console.log("click");
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(largeImgRef);
+  const largeImgURL = event.target.dataset.sourse;
+  largeImgRef.src = largeImgURL;
 }
