6b25c41a1792756adfafd215ea595f053213a3c6
Oksana Chorna <ksuchorna@gmail.com>
2021-01-31 21:32:12 +0200

    Зроблено (без сладера)

--- main-js.js
+++ main-js.js
@@ -65,22 +65,3 @@ function onPresEsc(event) {
     onCloseModal();
   }
 }
-
-// function onLeftArrow(event) {
-//   //   if (event.code === "ArrowLeft") {
-//   //     lightboxImgRef
-//   //   }
-//   galleryRef.();
-// }
-
-// const img = (img) => {
-//   return galleryRef.map((img) => galleryRef.img);
-// };
-
-// }
-// function onRightArrow(event){
-//  if (event.code === "ArrowRight") {}}
-// const getUserNames = (users) => {
-//   // твой код
-//   return users.map((user) => user.name);
-// };
